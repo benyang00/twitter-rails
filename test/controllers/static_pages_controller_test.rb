7c254e5dd613e3eca62d5684017a94c9e9d93fad
Prima Aulia Gusta <primaulia@gmail.com>
2016-07-26 11:49:24 +0800

    latest commit

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
 end
